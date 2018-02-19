42afe4a5a86bce2a75c7d7b7e96fb9891472a221
Dexter Slusarski <dexter.slusarski@gmail.com>
2018-02-19 13:26:57 -0500

    update time stamp

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@ import Controller from './controller.class.js';
 import Connector from './connector.class.js';
 (function(){
   let controller = new Controller(
-    1514782800000, 1518757199000, {
+    1514782800000, 1519837199000, {
     styleURL: 'mapbox://styles/mapbox',
     mapContainer: 'map',
     geocoder: false,
